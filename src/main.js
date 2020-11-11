eff020fb3a73cd78b7775fba1bf36c4aad9a7f2d
linzhengxi <13725534983@163.com>
2020-11-11 16:16:14 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import reactive from './recative'
+import reactive from './reactive'
 import Watcher from './watcher'
 import computed from './computed.js'
 import watch from './watch'
--- src/reactive.js
+++ src/reactive.js
@@ -1,7 +1,7 @@
 import Dep from './dep';
 import { isObject } from './utils'
 
-export default function recative(data) {
+export default function reactive(data) {
     if (isObject(data)) {
         Object.keys(data).forEach((key) => {
             defaultReactive(data, key)
@@ -24,6 +24,6 @@ function defaultReactive(data, key) {
         }
     })
     if (isObject(val)) {
-        recative(val)
+        reactive(val)
     }
 }
